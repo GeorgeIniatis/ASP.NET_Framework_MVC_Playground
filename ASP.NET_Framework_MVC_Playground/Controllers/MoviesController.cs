901a9a0120091a0d65b85a1af043a517c94e3977
George Iniatis <jogeo98@hotmail.com>
2022-07-27 14:12:30 +0300

    Removed About Page - Integrated About page with Home page - Random page CSS fix

--- ASP.NET_Framework_MVC_Playground/Controllers/MoviesController.cs
+++ ASP.NET_Framework_MVC_Playground/Controllers/MoviesController.cs
@@ -25,11 +25,6 @@ namespace ASP.NET_Framework_MVC_Playground.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            return View();
-        }
-
         [Authorize(Roles = "Admin")]
         public ActionResult EditMovieDetails(int id)
         {
